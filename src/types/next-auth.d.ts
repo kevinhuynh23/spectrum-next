e65b30def817529f4080d7d3e47c930802a74a0a
Kevin Huynh <kevhuyn23@gmail.com>
2026-05-10 18:45:04 -0700

    chore: update next-auth type augmentation for v5

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,18 +1,16 @@
 import 'next-auth'
-import 'next-auth/jwt'
 
 declare module 'next-auth' {
   interface Session {
     user: {
       id: string
-      username: string
       email: string
-      name?: string | null
-      image?: string | null
+      name: string
+      username: string
     }
   }
   interface User {
-    username: string
+    username?: string
   }
 }
 
