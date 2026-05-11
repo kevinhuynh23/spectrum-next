fe53360e39df3ea4843c2500704109498d3757e0
Kevin Huynh <kevhuyn23@gmail.com>
2026-05-10 17:55:06 -0700

    chore: configure Tailwind v4 + HeroUI plugin

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { heroui } from '@heroui/react'
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
+  darkMode: 'class',
+  plugins: [heroui()],
+}
+
+export default config
